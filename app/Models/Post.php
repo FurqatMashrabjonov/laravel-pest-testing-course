4181950ddd1afd3801985b25a4df874c23c32d71
FurqatMashrabjonov <php_lesson@mail.ru>
2023-12-09 23:32:24 +0500

    Testing unit Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,7 +12,8 @@ class Post extends Model
 
     protected $fillable = [
         'title',
-        'description'
+        'description',
+        'comment'
     ];
 
     public function likes(): HasMany
