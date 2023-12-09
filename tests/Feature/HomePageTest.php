ca4b8176229c8fc3d386209f7d818fa703801d8a
FurqatMashrabjonov <php_lesson@mail.ru>
2023-12-09 23:18:13 +0500

    Testing that Post model has 5 likes

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Models\Like;
 use App\Models\Post;
 
 test('it can render homepage', function (){
+    $post = Post::factory()
+        ->has(Like::factory()->count(5))
+//        ->hasLikes(5) //has{Relationship Method Name}
+    ->create();
 
-    $post = Post::factory()->create();
-
-    $this->get('/')
-        ->assertSee($post->title)
-        ->assertDontSee($post->title . ' fake');
+    expect($post->likes)->toHaveCount(5);
 });
 
